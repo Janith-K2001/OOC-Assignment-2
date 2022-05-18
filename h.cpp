5791e1037c67f9d62d1406df4c34be4f0cd5726e
Janith-K2001 <105011193+Janith-K2001@users.noreply.github.com>
2022-05-18 15:28:11 +0530

    Add files via upload

---
+++ h.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+class Employee{
+    private:
+      int EmpID;
+      char EmpName[100];
+      char Position[30];
+      Branch *br;
+      Customer cus*;
+    public:
+       Employee();
+       Employee(int id,char name[],char post,Branch *pbr){
+           EmpID = id;
+           strcpy(EmpName,name);
+           strcpy(Position,post);
+           br=pbr;
+       }
+       void DisplayEmpData(){
+           cout<<"Employee ID: "<<EmpID<<endl;
+           cout<<"Employee Name: "<<EmpName<<endl;
+           cout<<"Position: "<<Position<<endl;
+       }
+      
+
+};
+class Branch{
+    private:
+      int BranchID;
+      char BranchName[50];
+      char Location[50];
+      Employee *employee;
+    public:
+     Branch();
+     Branch(int id,char bname[],char loc[]){
+         BranchID= id;
+         strcpy(BranchName,bname);
+         strcpy(Location,loc);
+     }
+     void DiasplayBranchDetails(){
+       cout<<"Branch Id:"<<BranchID<<endl;
+       cout<<"Branchname:"<<Branchname<<endl;
+       cout<<"Location:"<<Location<<endl;
+     }
+     
+     
+};
+int main(){
+  
+}
